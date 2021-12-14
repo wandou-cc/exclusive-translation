f1c6e4db2663c699c19fbf802da538a1b3b3bc6b
chenchao <chenchao@izuche.com>
2021-12-14 16:02:09 +0800

    fix:压入队列

--- src/extension.js
+++ src/extension.js
@@ -3,6 +3,7 @@ let command = require('./command')
 
 function activate(context) {
 	context.subscriptions.push(command.keyTranslate);
+	context.subscriptions.push(command.replaceTranslate);
 }
 
 module.exports = {
